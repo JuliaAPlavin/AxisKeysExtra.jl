e9eb7b675352494a6b46b51424e60cae268b0ba8
Alexander Plavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -67,6 +67,29 @@ end
     @test named_axiskeys(sk) == (a=[:x, :y], b=10:12)
 end
 
+@testset "structarrays" begin
+    A = StructArray(a=KeyedArray(1:5; x=11:15), b=KeyedArray(10:10:50; x=11:15))
+    @test named_axiskeys(A) === (x=11:15,)
+    @test A.a == 1:5
+    @test A.x == 11:15
+    @test A[2].a == 2
+    @test A[x=2].b == 20
+    @test A[x=1:2].x == 11:12
+    @test A(x=13).a == 3
+    @test A(x= >=(13)).b == 30:10:50
+    @test A(x= >=(13)).x == 13:15
+
+    A = StructArray(a=KeyedArray([1 2]; x=[:x], y=11:12), b=KeyedArray([10 20]; x=[:x], y=11:12))
+    @test named_axiskeys(A) == (x=[:x], y=11:12)
+    @test A.a == [1 2]
+    @test A.x == [:x]
+    @test A[1, 2].a == 2
+    @test A[x=1].b == [10, 20]
+    @test A[x=1, y=2].b == 20
+    @test A[y=1:2].x == [:x]
+    @test A(x=:x, y=11).a == 1
+end
+
 @testset "axiskeys grid" begin
     A = KeyedArray([1 2 3; 4 5 6], a=-1:0, b=1:3)
     ak_g = axiskeys_grid(A)
@@ -79,6 +102,8 @@ end
     @test w_ak[2, 3] === ((a=0, b=3) => 6)
     @test w_ak.:1 == ak_g
     @test w_ak.:2 === A
+    @test named_axiskeys(w_ak) == (a=-1:0, b=1:3)
+    @test w_ak(a=0, b=3) === ((a=0, b=3) => 6)
 
     fA = filter(p -> p[1].a >= 0, w_ak)
     @test fA.:1 == vec(AxisKeysExtra.grid(a=0:0, b=1:3))
@@ -104,29 +129,6 @@ end
     @test with_axiskeys(findmax)(arr) == (6, (a=0, b=3))
     @test with_axiskeys(argmin)(arr) == (a=-1, b=1)
     @test with_axiskeys(findmin)(arr) == (1, (a=-1, b=1))
-end
-
-@testset "structarrays" begin
-    A = StructArray(a=KeyedArray(1:5; x=11:15), b=KeyedArray(10:10:50; x=11:15))
-    @test named_axiskeys(A) === (x=11:15,)
-    @test A.a == 1:5
-    @test A.x == 11:15
-    @test A[2].a == 2
-    @test A[x=2].b == 20
-    @test A[x=1:2].x == 11:12
-    @test A(x=13).a == 3
-    @test A(x= >=(13)).b == 30:10:50
-    @test A(x= >=(13)).x == 13:15
-
-    A = StructArray(a=KeyedArray([1 2]; x=[:x], y=11:12), b=KeyedArray([10 20]; x=[:x], y=11:12))
-    @test named_axiskeys(A) == (x=[:x], y=11:12)
-    @test A.a == [1 2]
-    @test A.x == [:x]
-    @test A[1, 2].a == 2
-    @test A[x=1].b == [10, 20]
-    @test A[x=1, y=2].b == 20
-    @test A[y=1:2].x == [:x]
-    @test A(x=:x, y=11).a == 1
 end
 
 
