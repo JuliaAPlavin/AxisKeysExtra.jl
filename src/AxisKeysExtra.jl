1c7d5ca411ee22564d68f2f4b267efc108e097d0
Alexander Plavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    actual implementation

--- src/AxisKeysExtra.jl
+++ src/AxisKeysExtra.jl
@@ -1,3 +1,69 @@
 module AxisKeysExtra
 
+using Reexport
+@reexport using AxisKeys
+using AxisKeys: keyless
+using RectiGrids
+using StructArrays
+import Base: eachslice
+
+export axiskeys_grid, with_axiskeys
+
+if VERSION > v"1.9-DEV"
+    import Base: stack
+else
+    import Compat: stack
+    export stack
+end
+
+if VERSION > v"1.9-DEV"
+    # fix for AxisKeys
+    Base.uncolon(inds, I) = Base.uncolon(inds)
+end
+
+@static if VERSION > v"1.9-DEV"
+    # AxisKeys defines their own eachslice method, so here we explicitly call the base AbstractArray one
+    _base_eachslice(A; dims) = @invoke eachslice(A::AbstractArray; dims)
+else
+    using SplitApplyCombine
+    _base_eachslice(A; dims) = splitdimsview(A, dims)
+end
+
+
+function stack(A::KeyedArray; dims::Colon=:)
+    data = Base.@invoke stack(A::AbstractArray; dims)
+    if !allequal(named_axiskeys(a) for a in A)
+        throw(DimensionMismatch("stack expects uniform axiskeys for all arrays"))
+    end
+    akeys = (; named_axiskeys(first(A))..., named_axiskeys(A)...)
+    KeyedArray(data; akeys...)
+end
+
+function eachslice(A::KeyedArray; dims)
+    dims_ix = AxisKeys.dim(A, dims) |> Tuple
+    data = _base_eachslice(A::AbstractArray; dims=dims_ix)
+    return KeyedArray(NamedDimsArray(data, map(d -> dimnames(A, d), dims_ix)), map(d -> axiskeys(A, d), dims_ix))
+end
+
+function eachslice(A::NamedDimsArray; dims)
+    dims_ix = AxisKeys.dim(A, dims) |> Tuple
+    data = _base_eachslice(A::AbstractArray; dims=dims_ix)
+    return NamedDimsArray(data, map(d -> dimnames(A, d), dims_ix))
+end
+
+
+axiskeys_grid(A) = grid(; named_axiskeys(A)...)
+function with_axiskeys(A)
+    G = axiskeys_grid(A)
+    StructArray{Pair{eltype(G), eltype(A)}}((G, A))
+end
+
+with_axiskeys(f::Union{typeof.((argmax, argmin))...}) = A -> ix_to_axiskeys(A, f(A))
+with_axiskeys(f::Union{typeof.((findmax, findmin))...}) = A -> let
+    (x, ix) = f(A)
+    (x, ix_to_axiskeys(A, ix))
+end
+
+ix_to_axiskeys(A, ix) = NamedTuple{dimnames(A)}(map((i, vs) -> vs[i], Tuple(ix), axiskeys(A)))
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,92 @@
 using AxisKeysExtra
+using AxisKeys: keyless_unname, keyless
 using Test
 
-@testset begin
+
+@testset "eachslice" begin
+    arr = KeyedArray([1 2 3; 4 5 6], a=-1:0, b=1:3)
+
+    # Base method for regular array
+    @test collect(eachslice(keyless_unname(arr); dims=1)) == [[1, 2, 3], [4, 5, 6]]
+
+    @testset "nameddims" begin
+        S = eachslice(keyless(arr); dims=1)::NamedDimsArray
+        @test S == [[1, 2, 3], [4, 5, 6]] 
+        @test dimnames(S) == (:a,)
+        @test dimnames(S[1]::NamedDimsArray) == (:b,)
+
+        S = eachslice(keyless(arr); dims=:a)::NamedDimsArray
+        @test S == [[1, 2, 3], [4, 5, 6]] 
+        @test dimnames(S) == (:a,)
+        @test dimnames(S[1]::NamedDimsArray) == (:b,)
+    end
+
+    @testset "keyedarray" begin
+        S = eachslice(arr; dims=1)
+        @test S == [[1, 2, 3], [4, 5, 6]]
+        @test S isa KeyedArray
+        @test S |> named_axiskeys == (a=-1:0,)
+        @test S[1] == [1, 2, 3]
+        @test S[1] isa KeyedArray
+        @test S[1] |> named_axiskeys == (b=1:3,)
+
+        S = eachslice(arr; dims=:a)
+        @test S == [[1, 2, 3], [4, 5, 6]]
+        @test S isa KeyedArray
+        @test S |> named_axiskeys == (a=-1:0,)
+        @test S[1] == [1, 2, 3]
+        @test S[1] isa KeyedArray
+        @test S[1] |> named_axiskeys == (b=1:3,)
+
+        @test eachslice(arr; dims=(:a, :b)) == fill.([1 2 3; 4 5 6])  # array of 0d arrays
+        @test eachslice(arr; dims=(:a, :b)) |> named_axiskeys == (a=-1:0, b=1:3)
+        @test eachslice(arr; dims=(:b, :a)) == fill.([1 4; 2 5; 3 6])  # array of 0d arrays
+        @test eachslice(arr; dims=(:b, :a)) |> named_axiskeys == (b=1:3, a=-1:0)
+        @test eachslice(arr; dims=(:b, 1)) == fill.([1 4; 2 5; 3 6])  # array of 0d arrays
+        @test eachslice(arr; dims=(:b, 1)) |> named_axiskeys == (b=1:3, a=-1:0)
+
+        @test_throws Exception eachslice(arr; dims=3)
+        @test_throws Exception eachslice(arr; dims=:c)
+        @test_throws Exception eachslice(arr; dims=(:c,))
+        @test_throws Exception eachslice(arr; dims=(:a, :c,))
+    end
+end
+
+@testset "stack" begin
+    arr = KeyedArray([
+        KeyedArray([1, 2], a=[:x, :y]),
+        KeyedArray([3, 4], a=[:x, :y]),
+        KeyedArray([5, 6], a=[:x, :y]),
+    ], b=10:12)
+
+    @test stack(keyless_unname(arr)) == [1 3 5; 2 4 6]
+    
+    sk = stack(arr)::KeyedArray
+    @test sk == [1 3 5; 2 4 6]
+    @test named_axiskeys(sk) == (a=[:x, :y], b=10:12)
+end
+
+@testset "axiskeys grid" begin
+    arr = KeyedArray([1 2 3; 4 5 6], a=-1:0, b=1:3)
+    ak_g = axiskeys_grid(arr)
+    @test ak_g isa AxisKeysExtra.RectiGrid
+    @test isconcretetype(eltype(ak_g))
+    @test ak_g == [(a=-1, b=1) (a=-1, b=2) (a=-1, b=3); (a=0, b=1) (a=0, b=2) (a=0, b=3)]
+
+    w_ak = with_axiskeys(arr)
+    @test isconcretetype(eltype(w_ak))
+    @test w_ak[2, 3] === ((a=0, b=3) => 6)
+    @test w_ak.:1 == ak_g
+    @test w_ak.:2 === arr
+end
+
+@testset "with_axiskeys funcs" begin
+    arr = KeyedArray([1 2 3; 4 5 6], a=-1:0, b=1:3)
+
+    @test with_axiskeys(argmax)(arr) == (a=0, b=3)
+    @test with_axiskeys(findmax)(arr) == (6, (a=0, b=3))
+    @test with_axiskeys(argmin)(arr) == (a=-1, b=1)
+    @test with_axiskeys(findmin)(arr) == (1, (a=-1, b=1))
 end
 
 
